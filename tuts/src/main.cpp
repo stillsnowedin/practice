260c776a8ee6e9aa3ce5ea1a29cb6111ef5b94ab
Kathryn Norris <kathrynanorris@gmail.com>
2014-11-19 22:36:16 -0800

    Created tuts project and added git ignore file.

---
+++ tuts/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tuts
+//
+//  Created by Kathryn on 11/19/14.
+//  Copyright (c) 2014 stillsnowedin. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
