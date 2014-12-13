7c0f63fb05e4f804c0c5103f1f6df848656dac94
Kathryn Norris <kathrynanorris@gmail.com>
2014-12-12 17:15:59 -0800

    Added glm library and drew a basic triangle.

--- tuts3D/src/main.cpp
+++ tuts3D/src/main.cpp
@@ -6,22 +6,74 @@
 //  Copyright (c) 2014 stillsnowedin. All rights reserved.
 //
 
-#include <iostream>
-#include "vector3d.h"
+#include <SDL2/SDL.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/gl3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+void init() {
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    
+    glm::mat4 projection = glm::perspective(45.0, 1024.0/768.0, 1.0, 500.0);
+    glLoadMatrixf(glm::value_ptr(projection));
+    
+    // if you're using the new shader based pipelines
+    //GLint projectionUniformLocation = ...;
+    //glUniformMatrix4fv(projectionUniformLocation, 1, GL_FALSE, glm::value_ptr(projection));
+    
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void display() {
+    glClearDepth(1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glEnableClientState(GL_COLOR_ARRAY);
+    
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 2.0f, -5.0f);
+    glVertex3f(-2.0f, -2.0f, -5.0f);
+    glVertex3f(2.0f, -2.0f, -5.0f);
+    glEnd();
+}
 
 int main(int argc, const char * argv[]) {
-    vector3d vec1;
-    vector3d vec2(1, 2, 3);
-    vector3d vec3(6, 5, 4);
-    std::cout << vec1 << vec2;
-    std::cout << vec2.length() << std::endl;
-    vec2.normalize();
-    std::cout << vec2;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
     
-    vector3d vec23 = vec2 + vec3;
-    std::cout << vec23;
-    vec23 += vec2;
-    std::cout << vec23;
+    SDL_Window* screen = SDL_CreateWindow("Tuts 3D!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(screen);
+    init();
+    
+    bool running = true;
+    Uint32 start;
+    SDL_Event event;
+    
+    while (running) {
+        start = SDL_GetTicks();
+        
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+            }
+        }
+        
+        display();
+        SDL_GL_SwapWindow(screen);
+        
+        if (1000/30 > SDL_GetTicks() - start) {
+            SDL_Delay(1000/30 - (SDL_GetTicks() - start));
+        }
+    }
+    SDL_Quit();
     
     return 0;
 }
